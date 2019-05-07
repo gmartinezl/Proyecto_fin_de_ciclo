b99d4b11ee14ae065a7c10415a6534d1b11723f7
Guillermo Martinez <guilleml94@gmail.com>
2019-05-07 13:42:31 +0200

    Login y enrutado SPA Angular hecho

--- videoplay-app/src/app/app.module.ts
+++ videoplay-app/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { PlayerComponent } from './player/player.component';
+import { LoginComponent } from './usuarios/login/login.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { FavoritosComponent } from './favoritos/favoritos.component';
+
+//constante con todas las rutas de nuestra app.
+const routes: Routes = [
+  {path: '' ,redirectTo:'', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'favoritos', component: FavoritosComponent},
+  {path: 'noticias', component: NoticiasComponent},
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PlayerComponent,
+    LoginComponent,
+    NoticiasComponent,
+    FavoritosComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
