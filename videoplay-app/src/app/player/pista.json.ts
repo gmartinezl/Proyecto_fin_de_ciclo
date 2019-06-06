1cb54815450929a0fee4c97fd6253f027d12df74
gmartinezl <guilleml94@gmail.com>
2019-06-06 17:34:37 +0200

    player funcional

---
+++ videoplay-app/src/app/player/pista.json.ts
@@ -0,0 +1,5 @@
+import {Cancion} from'../canciones/cancion';
+
+export const CANCION: Cancion[] = [
+    {id: 1, titulo:'../../assets/music/track01.mp3', artista:'eddy', duracion: '0.33'}
+]
--- videoplay-app/src/app/player/player.component.ts
+++ videoplay-app/src/app/player/player.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 //import { PlayerService } from './player.service';
 import { Cancion } from '../canciones/cancion';
+import { $ } from 'protractor';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { CANCION } from './pista.json';
 
 @Component({
     selector: 'app-player',
@@ -13,11 +18,24 @@ export class PlayerComponent implements OnInit{
 
     pistas: Cancion[];
 
-    constructor(/*private playerService: PlayerService*/){}
+    constructor(private http: HttpClient/*private playerService: PlayerService*/){}
     ngOnInit(): void {
       /* this.playerService.getCanciones().subscribe(
            pistas => this.pistas = pistas
        );*/
+        this.pistas = CANCION;
+        console.log(this.pistas);
+       
+
+       
     }
+
+    play(): void{
+        let audio = new Audio();
+        audio.src = "../../assets/music/track01.mp3";
+        audio.load();
+        audio.play();
+    }
+  
    
 }
